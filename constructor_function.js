eefa4df9fc3c8807671fb2498d7adc2e756b988a
Sayed Al Mamur <117765267+13-Sam@users.noreply.github.com>
2023-06-06 16:47:24 +0600

    Update constructor_function.js

--- constructor_function.js
+++ constructor_function.js
@@ -1,8 +1,11 @@
 'use strict';
 const Person = function (firstName, birthYear) {
+  //Instance Properties
   this.firstName = firstName;
   this.birthYear = birthYear;
 };
+//Instance
 const jonas = new Person('Jonas', 1999);
 console.log(jonas);
+//instanceof
 console.log(jonas instanceof Person);
